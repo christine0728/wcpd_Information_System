e0deecc25a48c0bcf50136cbad0e0e5d268a9a53
christine0728 <christinebalbin28@gmail.com>
2024-04-25 16:00:29 +0800

    validation and regex

--- resources/views/investigator/investigator_victimsmngt.blade.php
+++ resources/views/investigator/investigator_victimsmngt.blade.php
@@ -79,11 +79,11 @@
                             <form action="filter-victimsmngt" method="GET">
                                 <div class="date-filter">
                                     <label for="start_date">From:</label>&nbsp;&nbsp;
-                                    <input type="date" name="start_date" class="form-control" id="start_date" value="{{ $start_date ?? old('start_date') }}">&nbsp;&nbsp;
+                                    <input type="date" name="start_date" class="form-control" id="start_date" value="{{ $start_date ?? old('start_date') }}" required>&nbsp;&nbsp;
                                     <label for="end_date">To:</label>&nbsp;&nbsp;
-                                    <input type="date" class="form-control" name="end_date" id="end_date" value="{{ $end_date ?? old('end_date') }}">&nbsp;&nbsp;
+                                    <input type="date" class="form-control" name="end_date" id="end_date" value="{{ $end_date ?? old('end_date') }}" required>&nbsp;&nbsp;
                                     <button type="submit" class="form-buttons" style="width: 20rem">Apply Filter</button>&nbsp;&nbsp;
-                                    <a href=" "><button type="button" class="link-buttons" style="background-color: #48145B">All</button></a>
+                                    <!-- <a href=" "><button type="button" class="link-buttons" style="background-color: #48145B">All</button></a> -->
                                 </div>
                             </form>
                         </div>
